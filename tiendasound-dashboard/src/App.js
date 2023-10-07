9e1408ffb766df26ef6f3d1d9a7f8bc42425bebe
agustinalima <agustinalima3089@gmail.com>
2023-10-07 20:42:55 -0300

    css en sidebar

--- tiendasound-dashboard/src/App.js
+++ tiendasound-dashboard/src/App.js
@@ -4,13 +4,11 @@ import Dashboard from './pages/Dashboard';
 import Productos from './pages/Productos';
 import Categorias from './pages/Categorias';
 import {Route, Routes} from 'react-router-dom';
-import Login from './pages/Login';
 
 
 function App() {
   return (
     <div className='body'>
-      <Login />
       <Sidebar />
 
       <div className='main-content'>
--- tiendasound-dashboard/src/components/Sidebar.js
+++ tiendasound-dashboard/src/components/Sidebar.js
@@ -5,15 +5,17 @@ function Sidebar(){
     return(
         <div className='sidebar'>
 
+            <div className='logo-container'>
             <img src="https://res.cloudinary.com/dlf8flk1o/image/upload/v1696704065/dashboard/logo_w52p4b.svg" alt=""></img>
+            </div>
             <nav>
                 <ul>
-                <li> <Link to="/dashboard">Dashboard</Link> </li>
-                <li> <Link to="/productos">Productos</Link> </li>
-                <li> <Link to="/categorias">Categorías</Link> </li>
+                <li><img src="https://res.cloudinary.com/dlf8flk1o/image/upload/v1696719276/dashboard/Icon-1_rilar4.svg" alt=""></img><Link to="/dashboard">Dashboard</Link> </li>
+                <li><img src="https://res.cloudinary.com/dlf8flk1o/image/upload/v1696719276/dashboard/Icon_sels5y.svg" alt=""></img><Link to="/productos">Productos</Link> </li>
+                <li><img src="https://res.cloudinary.com/dlf8flk1o/image/upload/v1696719277/dashboard/Vector_mobfob.svg" alt=""></img> <Link to="/categorias">Categorías</Link> </li>
                 </ul>
 
-                <span>Log out</span>
+                <span className='logout'><img src="https://res.cloudinary.com/dlf8flk1o/image/upload/v1696721682/dashboard/logout_tnsdpl.svg" alt=""></img>Log out</span>
             </nav>
         </div>
 
